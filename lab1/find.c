545aa697b7339bb5cf3922229c353f96f8709e4d
fro11o <tcps195283@gmail.com>
2021-02-20 20:57:56 +0800

    find.c (not finish yet)

---
+++ lab1/find.c
@@ -0,0 +1,97 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+int
+isMatch(char *s, char*pat)
+{
+    int i, j, find;
+
+    if (strlen(s) < strlen(pat)) {
+        return 0;
+    }
+    for (i = 0; i < strlen(s) - strlen(pat); ++i) {
+        find = 1;
+        for (j = 0; j < strlen(pat); ++j) {
+            if (s[i+j] != pat[j]) {
+                find = 0;
+                break;
+            }
+        }
+        if (find) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void
+find(char *path, char *pat)
+{
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if((fd = open(path, 0)) < 0){
+        fprintf(2, "find: cannot open %s\n", path);
+        return;
+    }
+
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+
+    switch(st.type){
+    case T_FILE:
+        if (isMatch(path, pat)) {
+            printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
+        }
+        break;
+
+    case T_DIR:
+        if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+            printf("find: path too long\n");
+            break;
+        }
+        strcpy(buf, path);
+        p = buf+strlen(buf);
+        *p++ = '/';
+        while(read(fd, &de, sizeof(de)) == sizeof(de)){
+            if(de.inum == 0)
+                continue;
+            memmove(p, de.name, DIRSIZ);
+            p[DIRSIZ] = 0;
+            if(stat(buf, &st) < 0){
+                printf("ls: cannot stat %s\n", buf);
+                continue;
+            }
+            if (st.type == T_DIR) {
+                find(path, pat);
+            } else {
+                if (isMatch(path, pat)) {
+                    printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+                }
+            }
+        }
+        break;
+    }
+    close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+    int i;
+
+    if(argc < 3){
+        find(".", "");  // print all entries
+    } else {
+        for(i=1; i<argc-1; i++)
+        find(argv[i], argv[argc-1]);
+    }
+    exit(0);
+}
